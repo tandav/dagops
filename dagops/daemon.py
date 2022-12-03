defbfb50e4cfac07599c65aa734354e6a2f1abcf
Alexander Rodionov <tandav@tandav.me>
2022-12-03 19:20:32 +0300

    wip

--- dagops/daemon.py
+++ dagops/daemon.py
@@ -20,9 +20,9 @@ dotenv.load_dotenv()
 class AsyncWatcher:
     def __init__(self, watch_path: str):
         self.watch_path = watch_path
-        self.queue = asyncio.Queue()
+        self.pending_queue = asyncio.Queue()
         # self.processes = {}
-        self.tasks = {}
+        self.tasks = {} # task_id: Task
         self.running_tasks = {}
         # self.logs_handlers = {}
         self.extraredis = ExtraRedisAsync(decode_responses=True)
@@ -72,11 +72,13 @@ class AsyncWatcher:
                 del self.tasks[task_id]
             await asyncio.sleep(1)
 
-    async def create_task(self, file: str) -> Task:
-        cmd = sys.executable, '-u', 'write_to_mongo.py'
-        env={'TASK_NAME': file}
-        task = ShellTask(cmd, env)
-        return task
+    # async def create_task(self, file: str) -> Task:
+    #     cmd = sys.executable, '-u', 'write_to_mongo.py'
+    #     env={'TASK_NAME': file}
+    #     task = ShellTask(cmd, env)
+    #     return task
+
+        
         # await self.state.set_task_status(task.id, TaskStatus.RUNNING)
         # await self.state.redis.hset(f'{self.state.prefix_tasks}:{task.id}', mapping={
         #     'status': TaskStatus.RUNNING,
@@ -113,32 +115,39 @@ class AsyncWatcher:
         # task = self.tasks[task_id]
         # self.running_tasks[task_id] = asyncio.create_task(task.run())
 
+
     async def stop_task(self):
         pass
+
             
     # async def update_files_tasks(self, files: Iterable[str]) -> None:
     async def update_files_tasks(self) -> None:
         """create tasks for new files"""
-        files = await self.state.get_files()
-        files_tasks = await self.state.files_tasks(files)
-        to_update = {}
-        for file, task_id in files_tasks.items():
-            if task_id is None:
-                task = await self.create_task(file)
-                self.tasks[task.id] = task
-                to_update[file] = task.id
-        if len(to_update) == 0:
-            return
-        await self.extraredis.mset(self.state.FILE_TASK_PREFIX, to_update)
-        await self.extraredis.redis.sadd(self.state.TASK_SET, *to_update.values())
-        await self.extraredis.mhset_field(self.state.TASK_PREFIX, 'status', dict.fromkeys(to_update.values(), TaskStatus.PENDING))
+        while True:
+            files = await self.state.get_files()
+            files_tasks = await self.state.files_tasks(files)
+            to_update = {}
+            for file, task_id in files_tasks.items():
+                if task_id is None:
+                    task = ShellTask(command=[sys.executable, '-u', 'write_to_mongo.py'], env={'TASK_NAME': file})
+                    self.tasks[task.id] = task
+                    to_update[file] = task.id
+            if len(to_update) == 0:
+                await asyncio.sleep(1)
+                continue
+            await self.extraredis.mset(self.state.FILE_TASK_PREFIX, to_update)
+            await self.extraredis.redis.sadd(self.state.TASK_SET, *to_update.values())
+            await self.extraredis.mhset_field(self.state.TASK_PREFIX, 'status', dict.fromkeys(to_update.values(), TaskStatus.PENDING))
+            for task in to_update.values():
+                await self.pending_queue.put(task)
+            await asyncio.sleep(1)
 
 
     async def watch_directory(self):
         while True:
             files = set(await aiofiles.os.listdir(self.watch_path))
             await self.state.update_files(files)
-            await self.update_files_tasks()
+            # await self.update_files_tasks()
             await asyncio.sleep(1)
  
 
@@ -154,68 +163,25 @@ class AsyncWatcher:
 
 
     async def process_tasks(self):
-        # reinit state
-
-        # async with asyncio.TaskGroup() as tg:
-        #     files = tg.create_task(aiofiles.os.listdir(self.watch_path))
-        #     statuses = tg.create_task(self.state.get_task_statuses())
-        # files = files.result()
-        # statuses = statuses.result()
-
-        # statuses = await redis.mget(keys)
-
-
-        # statuses = await self.extraredis.mhget_field(self.TASK_TABLE, 'status')
-        # for task_id, status in statuses.items():
-        #     if status in {TaskStatus.PENDING, TaskStatus.RUNNING}:
-        #         statuses[task_id] = TaskStatus.CANCELED
-        # await self.extraredis.mhset_field(self.TASK_TABLE, 'status', statuses)
-
         await self.cancel_orphaned_tasks()
-        
-        # f
 
-        # await self.state.set_task_status(task_id, TaskStatus.CANCELED)
-        # print(2)
-        # state = dict(zip(keys, statuses))
         while True:
             print('watching', self.watch_path)
-
-            # async with asyncio.TaskGroup() as tg:
-                # statuses = tg.create_task(self.state.tasks_statuses())
-            # statuses = statuses.result()
-            # dir_files = dir_files.result()
-            # new_files = await self.state.extraredis.redis
-            # files_tasks = await self.extraredis.mget('file_status', files)
-            # TODO: use set difference
-            # new_files = set(dir_files) - set(statuses)
-            # new_pending = dict.fromkeys(new_task_ids, TaskStatus.PENDING)
-            # print('new_pending', new_pending)
-            # await self.state.mset_task_status(new_pending)
-
-            # statuses, dir_tasks = await self.state.get_task_statuses()
-            # new_task_ids = await aiofiles.os.listdir(self.watch_path)
-            # self.state.mset_task_status({k: TaskStatus.PENDING for k in new_keys})
             statuses = await self.state.tasks_statuses()
             for task_id, status in statuses.items():
                 if status in {TaskStatus.PENDING, TaskStatus.RUNNING}:
                     print(task_id, status)
-                # if status is None:
-                #     await self.redis.set(key, TaskStatus.PENDING)
                 if status == TaskStatus.PENDING:
                     await self.start_task(task_id)
-                # elif status == TaskStatus.RUNNING:
-                    # await self.add_shell_task(task_id)
-                    # self.queue.put_nowait(key)
-            # for file in await aiofiles.os.listdir(path):
-
             await asyncio.sleep(1)
     
+
     async def main(self):
         async with asyncio.TaskGroup() as tg:
             tg.create_task(self.watch_directory())
             tg.create_task(self.process_tasks())
             tg.create_task(self.process_handlers())
+            tg.create_task(self.update_files_tasks())
      
 
 
--- dagops/dag.py
+++ dagops/dag.py
@@ -4,9 +4,18 @@ import asyncio
 
 
 class Dag:
-    def __init__(self, graph: dict[Task, set[Task]]) -> None:
-        self.graph = graph
+    def __init__(
+        self,
+        graph: dict[Task, set[Task]],
+        queue: asyncio.Queue[Task],
+    ) -> None:
+        self.graph = graphlib.TopologicalSorter(graph)
+        self.graph.prepare()
+        self.queue = queue
 
     async def run(self) -> None:
-        for task in self.graph:
-            await task.run()
+        while self.graph.is_active():
+            for task in self.graph.ready():
+                await self.queue.put(task)
+            task = self.queue.get()
+            self.graph.done(task)
