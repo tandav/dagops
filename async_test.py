1f08e3a7f8b660c348b2c750cfd7e1c45f9bf55f
Alexander Rodionov <tandav@tandav.me>
2022-12-03 14:04:49 +0300

    [WIP] mvp with extraredis works

--- async_test.py
+++ async_test.py
@@ -1,22 +1,33 @@
 import asyncio
 import aiofiles.os
-from redis import Redis
 import sys
 import random
 import json
 import contextlib
+from collections.abc import Iterable
 from dagops.task import TaskStatus
+from dagops.task import ShellTask
+from dagops.task import Task
 from dagops import config
 from dagops.state import State
+import dotenv
+from extraredis._async import ExtraRedisAsync
+
+dotenv.load_dotenv()
+
 
 
 class AsyncWatcher:
     def __init__(self, watch_path: str):
         self.watch_path = watch_path
         self.queue = asyncio.Queue()
-        self.processes = {}
-        self.logs_handlers = {}
-        self.state  = State()
+        # self.processes = {}
+        self.tasks = {}
+        self.running_tasks = {}
+        # self.logs_handlers = {}
+        self.extraredis = ExtraRedisAsync(decode_responses=True)
+        self.state  = State(self.extraredis)
+   
 
 
     async def run_subprocess(self, cmd, env, logs_fh):
@@ -32,11 +43,11 @@ class AsyncWatcher:
 
     async def process_handlers(self):
         while True:
-            if not self.processes:
+            if not self.running_tasks:
                 await asyncio.sleep(1)
                 continue
-            print(self.processes)
-            task_to_task_id = {t: k for k, t in self.processes.items()}
+            print(self.running_tasks)
+            task_to_task_id = {t: k for k, t in self.running_tasks.items()}
             done, running = await asyncio.wait(task_to_task_id, return_when=asyncio.FIRST_COMPLETED)
             # assert len(done) == 1
             print('done', done)
@@ -48,78 +59,146 @@ class AsyncWatcher:
                 print(task_id, p, p.returncode)
                 status = TaskStatus.SUCCESS if p.returncode == 0 else TaskStatus.FAILED
                 await self.state.set_task_status(task_id, status)
-                self.logs_handlers[task_id].close()
-                del self.logs_handlers[task_id]
-                del self.processes[task_id]
+                # self.logs_handlers[task_id].close()
+                # del self.logs_handlers[task_id]
+                del self.running_tasks[task_id]
+                del self.tasks[task_id]
             await asyncio.sleep(1)
 
-    async def add_process(self, task_id):
-        # while True:
-        # key = await self.queue.get()
+    async def create_task(self, file: str) -> Task:
         cmd = sys.executable, '-u', 'write_to_mongo.py'
-        env={'TASK_NAME': task_id}
-        await self.state.set_task_status(task_id, TaskStatus.RUNNING)
-        await self.state.redis.hset(f'{self.state.prefix_tasks}:{task_id}', mapping={
-            'status': TaskStatus.RUNNING,
-            'cmd': json.dumps(cmd),
-            'env': json.dumps(env),
-        })
-        logs_fh = open(f'static/logs/{task_id}.txt', 'w')
-        self.logs_handlers[task_id] = logs_fh
-        task = asyncio.create_task(self.run_subprocess(cmd, env, logs_fh))
-        self.processes[task_id] = task
+        env={'TASK_NAME': file}
+        task = ShellTask(cmd, env)
+        return task
+        # await self.state.set_task_status(task.id, TaskStatus.RUNNING)
+        # await self.state.redis.hset(f'{self.state.prefix_tasks}:{task.id}', mapping={
+        #     'status': TaskStatus.RUNNING,
+        #     'cmd': json.dumps(cmd),
+        #     'env': json.dumps(env),
+        # })
+        # self.processes[task.id] = asyncio.create_task(task.run())
+
+
+        # key = await self.queue.get()
+        # logs_fh = open(f'static/logs/{task_id}.txt', 'w')
+        # self.logs_handlers[task_id] = logs_fh
+        # task = asyncio.create_task(self.run_subprocess(cmd, env, logs_fh))
+
             # await asyncio.sleep(1)
-            # status = random.choice([TaskStatus.SUCCESS, TaskStatus.FAILED])
+            # status = random.choice([TaskStatus.SUCCESS, TaskStatusf.FAILED])
             # await redis.set(key, status)
             # print(await redis.get(key))
             # self.queue.task_done()
 
+    async def start_task(self, task_id: str):
+        task = self.tasks[task_id]
+        await self.state.set_shell_task(task, TaskStatus.RUNNING)
+        self.running_tasks[task_id] = asyncio.create_task(task.run())
+        # self.running_tasks[task_id] = asyncio.create_task(task.run())
+        # task = self.tasks[task_id]
+        # self.running_tasks[task_id] = asyncio.create_task(task.run())
+
+            
+    # async def update_files_tasks(self, files: Iterable[str]) -> None:
+    async def update_files_tasks(self) -> None:
+        """create tasks for new files"""
+        files = await self.state.get_files()
+        files_tasks = await self.state.files_tasks(files)
+        to_update = {}
+        for file, task_id in files_tasks.items():
+            if task_id is None:
+                task = await self.create_task(file)
+                self.tasks[task.id] = task
+                to_update[file] = task.id
+        if len(to_update) == 0:
+            return
+        await self.extraredis.mset(self.state.FILE_TASK_PREFIX, to_update)
+        await self.extraredis.redis.sadd(self.state.TASK_SET, *to_update.values())
+        await self.extraredis.mhset_field(self.state.TASK_PREFIX, 'status', dict.fromkeys(to_update.values(), TaskStatus.PENDING))
+
+
     async def watch_directory(self):
-        # reinit state
-        # print(1)
-        # keys = await aiofiles.os.listdir(self.watch_path)
-        # statuses = await redis.mget(keys)
-        statuses = await self.state.get_task_statuses()
-        for task_id, status in statuses.items():
+        while True:
+            files = set(await aiofiles.os.listdir(self.watch_path))
+            await self.state.update_files(files)
+            await self.update_files_tasks()
+            await asyncio.sleep(1)
+ 
+
+    async def cancel_orphaned_tasks(self):
+        # tasks = await self.state.get_tasks()
+        # tasks_statuses = await self.extraredis.mhget_field(self.TASK_PREFIX, 'status', tasks)
+        tasks_statuses = await self.state.tasks_statuses()
+        tasks_to_cancel = {}
+        for task_id, status in tasks_statuses.items():
             if status in {TaskStatus.PENDING, TaskStatus.RUNNING}:
-                statuses[task_id] = TaskStatus.CANCELED
+                tasks_to_cancel[task_id] = TaskStatus.CANCELED
+        await self.extraredis.mhset_field(self.state.TASK_PREFIX, 'status', tasks_to_cancel)
+
+
+    async def process_tasks(self):
+        # reinit state
+
+        # async with asyncio.TaskGroup() as tg:
+        #     files = tg.create_task(aiofiles.os.listdir(self.watch_path))
+        #     statuses = tg.create_task(self.state.get_task_statuses())
+        # files = files.result()
+        # statuses = statuses.result()
+
+        # statuses = await redis.mget(keys)
+
+
+        # statuses = await self.extraredis.mhget_field(self.TASK_TABLE, 'status')
+        # for task_id, status in statuses.items():
+        #     if status in {TaskStatus.PENDING, TaskStatus.RUNNING}:
+        #         statuses[task_id] = TaskStatus.CANCELED
+        # await self.extraredis.mhset_field(self.TASK_TABLE, 'status', statuses)
+
+        await self.cancel_orphaned_tasks()
+        
+        # f
+
         # await self.state.set_task_status(task_id, TaskStatus.CANCELED)
-        await self.state.mset_task_status(statuses)
         # print(2)
         # state = dict(zip(keys, statuses))
         while True:
             print('watching', self.watch_path)
-            async with asyncio.TaskGroup() as tg:
-                statuses = tg.create_task(self.state.get_task_statuses())
-                dir_task_ids = tg.create_task(aiofiles.os.listdir(self.watch_path))
-            statuses = statuses.result()
-            dir_task_ids = dir_task_ids.result()
 
-            new_task_ids = set(dir_task_ids) - set(statuses)
-            new_pending = dict.fromkeys(new_task_ids, TaskStatus.PENDING)
-            print('new_pending', new_pending)
-            await self.state.mset_task_status(new_pending)
+            # async with asyncio.TaskGroup() as tg:
+                # statuses = tg.create_task(self.state.tasks_statuses())
+            # statuses = statuses.result()
+            # dir_files = dir_files.result()
+            # new_files = await self.state.extraredis.redis
+            # files_tasks = await self.extraredis.mget('file_status', files)
+            # TODO: use set difference
+            # new_files = set(dir_files) - set(statuses)
+            # new_pending = dict.fromkeys(new_task_ids, TaskStatus.PENDING)
+            # print('new_pending', new_pending)
+            # await self.state.mset_task_status(new_pending)
 
             # statuses, dir_tasks = await self.state.get_task_statuses()
             # new_task_ids = await aiofiles.os.listdir(self.watch_path)
             # self.state.mset_task_status({k: TaskStatus.PENDING for k in new_keys})
-            statuses = await self.state.get_task_statuses()
+            statuses = await self.state.tasks_statuses()
+            print('statuses', statuses)
             for task_id, status in statuses.items():
                 print(task_id, status)
                 # if status is None:
-                    # await redis.set(key, TaskStatus.PENDING)
+                #     await self.redis.set(key, TaskStatus.PENDING)
                 if status == TaskStatus.PENDING:
-                    await self.add_process(task_id)
+                    await self.start_task(task_id)
+                    # await self.add_shell_task(task_id)
                     # self.queue.put_nowait(key)
             # for file in await aiofiles.os.listdir(path):
 
-            await asyncio.sleep(2)
+            await asyncio.sleep(1)
     
     async def main(self):
-        await asyncio.gather(
-            self.watch_directory(),
-            self.process_handlers(),
-        )
+        async with asyncio.TaskGroup() as tg:
+            tg.create_task(self.watch_directory())
+            tg.create_task(self.process_tasks())
+            tg.create_task(self.process_handlers())
+     
 
 
 if __name__ == '__main__':
--- dagops/task.py
+++ dagops/task.py
@@ -3,28 +3,54 @@ import abc
 import logging
 import uuid
 import enum
+import asyncio
 
 # class TaskStatus(enum.Enum):
 class TaskStatus:
-    PENDING = b'PENDING'
-    RUNNING = b'RUNNING'
-    SUCCESS = b'SUCCESS'
-    FAILED = b'FAILED'
-    CANCELED = b'CANCELED'
+    PENDING = 'PENDING'
+    RUNNING = 'RUNNING'
+    SUCCESS = 'SUCCESS'
+    FAILED = 'FAILED'
+    CANCELED = 'CANCELED'
+
 
 class Task:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+
+        
     @abc.abstractmethod
-    def run(self):
+    async def run(self):
         ...
 
-    @property
-    @abc.abstractmethod
-    def status(self):
-        ...
 
-    @abc.abstractmethod
-    def handle_running(self):
-        ...
+    # @property
+    # @abc.abstractmethod
+    # def status(self):
+    #     ...
+
+    # @abc.abstractmethod
+    # def handle_running(self):
+    #     ...
+
+
+class ShellTask(Task):
+    def __init__(self, command: list[str], env: dict[str, str] | None = None):
+        super().__init__()
+        self.command = command
+        self.env = env or {}
+        self.logs_fh = open(f'static/logs/{self.id}.txt', 'w')
+
+    async def run(self):
+        p = await asyncio.create_subprocess_exec(
+            *self.command,
+            env=self.env,
+            stdout=self.logs_fh,
+            stderr=asyncio.subprocess.STDOUT,
+        )
+        await p.communicate()
+        self.logs_fh.close()
+        return p
 
 # class Task:
 #     def __init__(self, deps: list[str], **kwargs):
@@ -58,69 +84,69 @@ class Task:
 
 
 
-class ShellTask(Task):
-    def __init__(
-        self,
-        # **kwargs,
-        command: list[str],
-        env: dict[str, str] | None = None,
-        name: str | None = None,
-    ):
-        # super().__init__(deps, **kwargs)
-        # self.deps = deps
-        self.command = command
-        self.env = env
-        self.p = None
-        self.lines = []
-        self.name = name or str(uuid.uuid4())
-        self.fh = open(f'static/logs/{self.name}.txt', 'w')
-        # fh = logging.FileHandler(f'logs/{self.name}.txt')
-        # self.logger = logging.getLogger(self.name)
-        # self.logger.addHandler(fh)
+# class ShellTask(Task):
+#     def __init__(
+#         self,
+#         # **kwargs,
+#         command: list[str],
+#         env: dict[str, str] | None = None,
+#         name: str | None = None,
+#     ):
+#         # super().__init__(deps, **kwargs)
+#         # self.deps = deps
+#         self.command = command
+#         self.env = env
+#         self.p = None
+#         self.lines = []
+#         self.name = name or str(uuid.uuid4())
+#         self.fh = open(f'static/logs/{self.name}.txt', 'w')
+#         # fh = logging.FileHandler(f'logs/{self.name}.txt')
+#         # self.logger = logging.getLogger(self.name)
+#         # self.logger.addHandler(fh)
 
-    @classmethod
-    def from_path(cls, path: str, env: dict[str, str] | None = None):
-        return cls(command=['sh', path], env=env)
+#     @classmethod
+#     def from_path(cls, path: str, env: dict[str, str] | None = None):
+#         return cls(command=['sh', path], env=env)
         
-    def run(self):
-        print(2, self.env)
-        self.p = subprocess.Popen(
-            self.command, 
-            env=self.env, 
-            stdout=self.fh,
-            # stdout=self.logger,
-            # stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,  # redirect stderr to stdout
-            text=True,
-        )
-        print(3, self.env)
+#     def run(self):
+#         print(2, self.env)
+#         self.p = subprocess.Popen(
+#             self.command, 
+#             env=self.env, 
+#             stdout=self.fh,
+#             # stdout=self.logger,
+#             # stdout=subprocess.PIPE,
+#             stderr=subprocess.STDOUT,  # redirect stderr to stdout
+#             text=True,
+#         )
+#         print(3, self.env)
 
-    @property
-    def status(self):
-        # print(7, self.env)
-        if self.p is None:
-            # print('7.1', self.env)
-            return TaskStatus.PENDING
-        elif self.p.poll() is None:
-            # print('7.2', self.env)
-            return TaskStatus.RUNNING
-        elif self.p.returncode == 0:
-            # print('7.3', self.env)
-            return TaskStatus.SUCCESS
-        else:
-            # print('7.4', self.env)
-            return TaskStatus.FAILED
+#     @property
+#     def status(self):
+#         # print(7, self.env)
+#         if self.p is None:
+#             # print('7.1', self.env)
+#             return TaskStatus.PENDING
+#         elif self.p.poll() is None:
+#             # print('7.2', self.env)
+#             return TaskStatus.RUNNING
+#         elif self.p.returncode == 0:
+#             # print('7.3', self.env)
+#             return TaskStatus.SUCCESS
+#         else:
+#             # print('7.4', self.env)
+#             return TaskStatus.FAILED
 
-    def handle_running(self):
-        pass
-        # print(8, self.env)
-        # if self.status != TaskStatus.RUNNING:
-        #     raise RuntimeError(f'Task is not running, current status: {self.status}')
+#     def handle_running(self):
+#         pass
+#         # print(8, self.env)
+#         # if self.status != TaskStatus.RUNNING:
+#         #     raise RuntimeError(f'Task is not running, current status: {self.status}')
         
-        # if self.p is None:
-        #     raise RuntimeError('p is None')
+#         # if self.p is None:
+#         #     raise RuntimeError('p is None')
 
-        # for line in self.p.stdout: # this is blocking till complete, bad, need use something else
-        # #     # logging.info(line.rstrip())
-        #     print(line.rstrip())
-        #     self.lines.append(line)
+#         # for line in self.p.stdout: # this is blocking till complete, bad, need use something else
+#         # #     # logging.info(line.rstrip())
+#         #     print(line.rstrip())
+#         #     self.lines.append(line)
