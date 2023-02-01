68ca56ffaada76f37a53388d4c3e6b360e862e1a
Alexander Rodionov <tandav@tandav.me>
2023-02-02 01:45:32 +0400

    add tempdir for logs in tests

--- tests/pipeline_test.py
+++ tests/pipeline_test.py
@@ -20,11 +20,12 @@ def db():
     database.drop_all()
 
 
-def test_pipeline(db):
+def test_pipeline(db, tmpdir):
     with mock.patch.dict(
         os.environ, {
             'N_ITERATIONS': '1',
             'MAX_N_ALL_DONE': '10',
+            'LOGS_DIRECTORY': str(tmpdir),
         },
     ):
         subprocess.check_call([sys.executable, 'examples/main.py'])
