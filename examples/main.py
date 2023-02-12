ab10b44704d89ea44f793eb58c5ca6353c9ce976
Alexander Rodionov <tandav@tandav.me>
2023-02-12 21:33:11 +0400

    [kinda works] watch_type = 'redis' (#33)

--- examples/main.py
+++ examples/main.py
@@ -52,6 +52,7 @@ async def main():
             db=db,
             redis=redis,
             create_dag_func=create_dag,
+            watch_type=os.environ.get('WATCH_TYPE', 'filesystem'),
         )
 
         daemon2 = Daemon(
@@ -60,6 +61,7 @@ async def main():
             redis=redis,
             create_dag_func=create_batch_dag,
             batch=True,
+            watch_type=os.environ.get('WATCH_TYPE', 'filesystem'),
         )
 
         workers = await prepare_workers(db, redis)
