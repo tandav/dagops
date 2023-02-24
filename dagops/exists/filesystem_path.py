c54fad11ea7712a54106b8051b0ff8fe300b7ecd
Alexander Rodionov <tandav@tandav.me>
2023-02-24 16:46:37 +0400

    exists module fixes

--- dagops/exists/filesystem_path.py
+++ dagops/exists/filesystem_path.py
@@ -3,8 +3,15 @@ from pathlib import Path
 
 from dagops.constant import NOT_EXISTS_RETURNCODE
 
-if __name__ == '__main__':
+
+def main():
     if Path(sys.argv[1]).exists():
+        print(f'filesystem path exists: {sys.argv[1]!r}')
         raise SystemExit(0)
     else:
+        print(f'filesystem path not exists: {sys.argv[1]!r}')
         raise SystemExit(NOT_EXISTS_RETURNCODE)
+
+
+if __name__ == '__main__':
+    main()
--- dagops/exists/redis_key.py
+++ dagops/exists/redis_key.py
@@ -1,11 +1,18 @@
+import asyncio
 import sys
 
 from dagops.constant import NOT_EXISTS_RETURNCODE
 from dagops.dependencies import get_redis_cm
 
-if __name__ == '__main__':
+
+async def main():
     with get_redis_cm() as redis:
-        if redis.exists(sys.argv[1]):
+        if await redis.exists(sys.argv[1]):
+            print(f'redis key exists: {sys.argv[1]!r}')
             raise SystemExit(0)
         else:
+            print(f'redis key not exists: {sys.argv[1]!r}')
             raise SystemExit(NOT_EXISTS_RETURNCODE)
+
+if __name__ == '__main__':
+    asyncio.run(main())
